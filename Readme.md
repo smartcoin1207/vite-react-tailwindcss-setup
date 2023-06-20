3ff6af3efbca377d847a66a89eae98980172a98a
smartcoin1207 <jackle.some99@gmail.com>
2023-06-20 12:18:02 +0200

    Readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 <h1>VITE-REACT-TAILWINDCSS SETUP</h1>
 
 Setting up a Vite.js project with React-template and TailwindCSS<br>
+
+1. npm create vite@latest myprojectname -- --template react<br>
+2. cd myprojectname
+3. npm install -D tailwindcss postcss autoprefixer<br><br>
+
+Generate the Configuration Files<br><br>
+4. npx tailwindcss init -p
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import curry from '../assets/images/curry.jpg'
+import stew from '../assets/images/stew.jpg'
 
 const Recipes = () => {
   return (
     <div className="px-16 bg-gray-100">
       <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Latest Recipes</h4>
       <div className="mt-8">
+
         {/* cards go here */}
-        <div className="bg-white rounded overflow-hidden shadow-md">
-          <img src={curry} alt="curry" className="w-full h-32 sm:h-48 object-cover"/>
+
+        <div className="bg-white rounded overflow-hidden shadow-md relative">
+          <img src={stew} alt="curry" className="w-full h-32 sm:h-48 object-cover"/>
           <div className="m-4">
             <span className="font-bold">5 bean chili stew</span>
             <span className="block text-gray-500 text-sm">Recipe by Mario</span>
+          </div>
+          <div className="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
+            <span>25 mins</span>
           </div>
         </div>
       </div>
